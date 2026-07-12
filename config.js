47e3c7b0aae0acf8548b0f8916911bf252fb2b70
STEP3DLab <projects.step3d@gmail.com>
2026-07-13 00:52:08 +0300

    Remove obsolete style overrides

--- config.js
+++ config.js
@@ -1,10 +1,3 @@
 window.XB_APPS_SCRIPT_URL = '';
 window.XB_MUSIC_FILE = 'assets/ryazan-moya.mp3';
 window.XB_CONTACT_PHONE = '79969662393';
-document.documentElement.style.setProperty('--daisies-image', "url('assets/daisy-cutout.webp')");
-(() => {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = 'final.css';
-  document.head.appendChild(link);
-})();
